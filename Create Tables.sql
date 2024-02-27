350de61e65bcae8522cf7e0ccc966b934bdcce24
palberque <156721125+palberque@users.noreply.github.com>
2024-02-27 16:05:43 -0500

    Add files via upload

---
+++ Create Tables.sql
@@ -0,0 +1,65 @@
+---- Start by creating the database
+
+CREATE SCHEMA dannys_diner;
+use dannys_diner;
+
+
+-- We are starting fresh by droping the tables if they already exist priort to creation of the new table
+
+Drop Table if exists sales;
+
+CREATE TABLE sales (
+  `customer_id` VARCHAR(1),
+  `order_date` DATE,
+  `product_id` INTEGER
+);
+
+Drop Table if exists members;
+
+CREATE TABLE members (
+  `customer_id` VARCHAR(1),
+  `join_date` DATE
+);
+
+Drop Table if exists menu;
+
+CREATE TABLE menu (
+  `product_id` INTEGER,
+  `product_name` VARCHAR(5),
+  `price` INTEGER
+);
+
+
+---- Inserting the data into the previously crates tables.
+
+INSERT INTO sales
+  (`customer_id`, `order_date`, `product_id`)
+VALUES
+  ('A', '2021-01-01', 1),
+  ('A', '2021-01-01', 2),
+  ('A', '2021-01-07', 2),
+  ('A', '2021-01-10', 3),
+  ('A', '2021-01-11', 3),
+  ('A', '2021-01-11', 3),
+  ('B', '2021-01-01', 2),
+  ('B', '2021-01-02', 2),
+  ('B', '2021-01-04', 1),
+  ('B', '2021-01-11', 1),
+  ('B', '2021-01-16', 3),
+  ('B', '2021-02-01', 3),
+  ('C', '2021-01-01', 3),
+  ('C', '2021-01-01', 3),
+  ('C', '2021-01-07', 3);
+
+INSERT INTO menu
+  (`product_id`, `product_name`, `price`)
+VALUES
+  (1, 'sushi', 10),
+  (2, 'curry', 15),
+  (3, 'ramen', 12);
+
+INSERT INTO members
+  (`customer_id`, `join_date`)
+VALUES
+  ('A', '2021-01-07'),
+  ('B', '2021-01-09');
